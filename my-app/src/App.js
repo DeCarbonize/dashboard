0004a792d20341493735f1d353e9e72e4da5ef76
mlam3 <mlam3@cs.washington.edu>
2020-10-17 22:01:08 -0700

    Added variables

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import CountUp from 'react-countup';
 
 function App() {
+    var emissions = 4;
+    var miles = 2300;
+    var purchases = 10;
+    var offsetCost = 165;
 
   return (
     <div className="App">
@@ -12,27 +16,27 @@ function App() {
         <div id="stats">
             <div class="subStat">
                 <h1>
-                    <CountUp start={0} end={4} duration={2.75}/>
+                    <CountUp start={0} end={emissions} duration={2.75}/>
                 </h1>
                 <p>Tons of carbon emissions produced</p>
             </div>
 
             <div class="subStat">
                 <h1>
-                    <CountUp start={0} end={2300} duration={2.75}/>
+                    <CountUp start={0} end={miles} duration={2.75}/>
                 </h1>
                 <p>Total miles traveled</p>
             </div>
 
             <div class="subStat">
                 <h1>
-                    <CountUp start={0} end={10} duration={2.75}/>
+                    <CountUp start={0} end={purchases} duration={2.75}/>
                 </h1>
                 <p>Total number of purchases</p>
             </div>
         </div>
           <div id="offset">
-              <h2>Cost to offset your emissions: $<CountUp start={0} end={165} duration={2.75}/></h2>
+              <h2>Cost to offset your emissions: $<CountUp start={0} end={offsetCost} duration={2.75}/></h2>
           </div>
       </header>
     </div>
